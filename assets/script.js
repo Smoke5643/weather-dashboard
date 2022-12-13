a0191b7bc68fe3800d899f5fd28d26ba8a3f7623
Smoke5643 <smoke5643@gmail.com>
2022-12-12 20:53:57 -0500

    redo of api url and tested getting cooridnates to use in next function

--- assets/script.js
+++ assets/script.js
@@ -3,35 +3,29 @@ var input = document.querySelector('#user-search')
 var searchBtn = document.querySelector('.searchButton');
 var currentCard = document.querySelector('.currentWeather')
 
-var getWeatherData = function(w) {    
-    var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather/?q=' + w + '&units=imperial&APPID=' + apiKey;
+var startSearch = function(event) {
+    event.preventDefault();
+    var w = input.value.trim();
+    getCode(w);
+}
+
+var getCode = function(w){
+    var codeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + w + "&limit=1&appid=" + apiKey;
     
-    fetch(openWeatherUrl)
+    fetch(codeURL)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-                // showWeather(w.data)
+                
             });
         }
     })
     // .catch(function (error) {
-        //     alert('Unable to connect');
-        // });
-    }
-
-    var startSearch = function(event) {
-        event.preventDefault();
-        var w = input.value.trim();
-        getWeatherData(w);
-    }
+            
+    // });
     
-  
-
-
-
-
-
+}
 
 
 
